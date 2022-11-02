26552ecdbd64655d4c99623164bd6ee9e4c5c8e9
Alyssa <alyssa.black141@gmail.com>
2022-11-02 16:36:15 -0600

    finished both beginner and intermediate task

--- js-interactivity/index.js
+++ js-interactivity/index.js
@@ -1,1 +1,52 @@
-console.log('hello')
+// console.log('hello') //to check connectivity
+
+let message = document.querySelector('#message')
+
+const addMovie = (event) =>{
+    event.preventDefault()
+
+    let inputField = document.querySelector('input');
+    let movie = document.createElement('li')
+    let movieTitle = document.createElement('span');
+    
+    
+    movieTitle.textContent = inputField.value
+
+    movieTitle.addEventListener('click', crossOffMovie)
+
+    movie.appendChild(movieTitle)
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'X'
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+
+    document.querySelector('ul').appendChild(movie)
+    inputField.value = ''
+}
+
+
+const deleteMovie = (event) => {
+    event.target.parentNode.remove() 
+    message.textContent = `You've deleted ${event.target.parentNode.childNodes[0].textContent}`
+    
+    revealMessage()
+}
+
+const crossOffMovie = (event) => {
+    event.target.classList.toggle('checked') 
+    if (event.target.classList.contains('checked') ){
+        message.textContent = `You've watched ${event.target.textContent}.`
+    } else {
+        message.textContent = `You've unwatched ${event.target.textContent}.`
+    }
+}
+
+const revealMessage = () => {
+    message.classList.remove('hide')
+    setTimeout(callback = () => {
+        message.classList.add('hide')
+    }, 1000);
+}
+
+document.querySelector('form').addEventListener('submit', addMovie) 
